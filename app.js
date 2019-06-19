c6f5f1ad2fbfd938aa25ae93873bf7c74f61e0f8
Carly Cox <webdevbarbie@gmail.com>
2019-06-19 16:46:47 -0500

    add /family /articles/:postID routes

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/barbieDB", {useNewUrlParser:true});
+
+const articleSchema = {
+  topic: String,
+  title: String,
+  content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
 
 // ------------routes------------
 
@@ -21,8 +30,28 @@ app.get("/", function(req, res){
 });
 
 app.get("/family", function(req, res){
-  res.render("family");
-})
+  Article.find({topic: "Family"}, function(err, foundFamily){
+    if (!err) {
+      res.render("family", {familyMember:foundFamily});
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.get("/articles/:postID", function(req, res){
+
+const requestedPostId = req.params.postID;
+
+  Article.findOne({_id: requestedPostId}, function(err, article){
+    res.render("article", {
+      title: article.title,
+      content: article.content
+    });
+  });
+
+});
+
 
 
 app.listen(3000, function() {
