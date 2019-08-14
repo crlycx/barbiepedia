add8c667e45bb83a72356a34893c903d736468f9
Carly Cox <webdevbarbie@gmail.com>
2019-08-14 13:33:44 -0500

    add Procfile, update ports

--- app.js
+++ app.js
@@ -149,7 +149,11 @@ app.post("/corrections", function(req, res){
 
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started successfully.");
 });
